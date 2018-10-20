de51ba1eabba32c0e87cf591116a953ba7fc2e3e
kaleong kong <kaleongkong@yahoo.com>
2018-10-20 11:32:04 -0700

    fix heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.url = "wss://funbig2apis.herokuapp.com/cable"
 end
