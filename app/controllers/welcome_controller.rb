87bb3228add1339195253636080f069fd08d2bce
kaleong kong <kaleongkong@yahoo.com>
2018-10-20 21:53:04 -0700

    moves

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,13 @@
 class WelcomeController < ApplicationController
+  protect_from_forgery with: :null_session
   $room = {players:{}}
   def join_room
     $room[:players][params[:user]] = true if params[:user]
     render :json => {start_state: ($room[:players].length == 2 ? 1 : -1)}
+  end
+
+  def move
+    render :json => params[:combination]
   end
 
   def reset
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'welcome/reset'
 
+  post 'welcome/move'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
