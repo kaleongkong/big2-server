1fc67a0b599b9f3b5b16359c3b9c1f08015efc1f
Kaleong Kong <kaleongkong@macbook-pro.lan>
2018-11-20 23:59:25 -0800

    add new ip for new mac

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
-  Rails.application.config.action_cable.allowed_request_origins = ['http://192.168.86.134:3000', 'http://localhost:3000']
+  Rails.application.config.action_cable.allowed_request_origins = ['http://192.168.86.134:3000', 'http://localhost:3000', 'http://192.168.86.181:3000']
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
